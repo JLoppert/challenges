68e9588f17ca06d26ad4112f1c72014898f6a91b
Jeffrey Loppert <Jeffrey.Loppert@outlook.com>
2016-02-12 00:55:08 -0500

    updated constants file

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,2 +1,7 @@
 SODA_DOMAIN = 'data.cityofboston.gov'
-SODA_API_TOKEN = 'H8FYuzRgWp0soSR319y4TVp8U'
+SODA_API_TOKEN = ENV['SODA_API_TOKEN']
+
+EARNINGS_REPORT_YEARS = [ 2011, 2012, 2013, 2014 ]
+EARNINGS_REPORT_RESOURCES = [ '9yt8-rqxb', 'wypd-uw2t', '5kqy-n8eq', 'ntv7-hwjm' ]
+
+MARKDOWN_FILE_PATH = "#{Rails.root}/README.md"
